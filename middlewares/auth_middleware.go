cda114f041882d9aaf60ccf4facb73af57327d04
riffani <sidikriffani481@gmail.com>
2026-04-06 09:15:13 +0700

    feat(backend):add middleware to secure endpoint

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,51 @@
+package middlewares
+
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	"garage-api/helpers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		authHeader := c.GetHeader("Authorization")
+		
+
+		if authHeader == "" {
+			helpers.ErrorResponse(c, http.StatusUnauthorized, "Authorization header required", nil)
+			c.Abort()
+			return
+		}
+
+		// format: Bearer TOKEN
+		tokenString := strings.Split(authHeader, " ")
+
+		if len(tokenString) != 2 {
+			helpers.ErrorResponse(c, http.StatusUnauthorized, "Invalid token format", nil)
+			c.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString[1], func(token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		if err != nil || !token.Valid {
+			helpers.ErrorResponse(c, http.StatusUnauthorized, "Invalid token", nil)
+			c.Abort()
+			return
+		}
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.Set("user_id", claims["user_id"])
+		}
+
+		c.Next()
+	}
+}
--- routes/vehicle_routes.go
+++ routes/vehicle_routes.go
@@ -3,30 +3,26 @@ package routes
 import (
 	"garage-api/controllers"
 	"github.com/gin-gonic/gin"
+	"garage-api/middlewares"
 )
 
 func SetupRoutes(r *gin.Engine, vc *controllers.VehicleController, sc *controllers.ServiceController, ac *controllers.AuthController) {
-	// vehicle
-	r.GET("/vehicles", vc.GetVehicles)
 
-	r.GET("/vehicles/:id", vc.GetVehicleById)
-
-	r.POST("/vehicles", vc.CreateVehicle)
-
-	r.PUT("/vehicles/:id", vc.UpdateVehicle)
-
-	r.DELETE("/vehicles/:id", vc.DeleteVehicle)
-
-	r.GET("/vehicles/:id/detail", vc.GetVehicleDetail)
-
-	// services
-	r.POST("/services", sc.CreateService)
-	r.GET("/services", sc.GetServices)
-
-	// relation
-	r.GET("/vehicles/:id/services", sc.GetServicesByVehicle)
-
-	// auth
+	// public route
 	r.POST("/auth/login", ac.Login)
 
+	// protected routes
+	auth := r.Group("/")
+	auth.Use(middlewares.AuthMiddleware())
+
+	// vehicles
+	auth.GET("/vehicles", vc.GetVehicles)
+	auth.POST("/vehicles", vc.CreateVehicle)
+	auth.PUT("/vehicles/:id", vc.UpdateVehicle)
+	auth.DELETE("/vehicles/:id", vc.DeleteVehicle)
+
+	// services
+	auth.POST("/services", sc.CreateService)
+	auth.GET("/services", sc.GetServices)
+	auth.GET("/vehicles/:id/services", sc.GetServicesByVehicle)
 }
